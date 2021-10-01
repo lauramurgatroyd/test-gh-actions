04a0e54e71597eeb43782571a46190c4888812a1
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-10-01 17:53:39 +0100

    Update test_version.py

--- test/test_version.py
+++ test/test_version.py
@@ -3,7 +3,7 @@ import unittest
 class TestModuleBase(unittest.TestCase):
     def test_version(self):
         try:
-            from hello-world import version
+            from hello_world import version
             a = version.version
             print ("version", a)
             self.assertTrue(isinstance(a, str))
