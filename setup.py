5c85644463866ff5db3e4dcb7409b96ce1a084e3
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-10-29 15:57:58 +0100

    Make version file in setup.py

--- setup.py
+++ setup.py
@@ -13,19 +13,60 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import os
+
+#!/usr/bin/env python3
 from distutils.core import setup
+import os
 import subprocess
+    
+def version2pep440(version):
+    '''normalises the version from git describe to pep440
+    
+    https://www.python.org/dev/peps/pep-0440/#id29
+    '''
+    if version[0] == 'v':
+        version = version[1:]
 
-cmd = 'git describe'
-dversion = subprocess.check_output(cmd, shell=True).strip().decode('utf-8')[1:]
+    if u'-' in version:
+        v = version.split('-')
+        v_pep440 = "{}.dev{}".format(v[0], v[1])
+    else:
+        v_pep440 = version
 
-print ('version {}'.format(dversion))
+    return v_pep440
+
+
+git_version_string = subprocess.check_output('git describe', shell=True).decode("utf-8").rstrip()[1:]
+    
+
+#with open("README.rst", "r") as fh:
+#    long_description = fh.read()
+long_description = 'A number of templates and tools to develop Qt GUIs with Python effectively.'
+
+if os.environ.get('CONDA_BUILD', 0) == '1':
+    # if it is a conda build requirements are going to be satisfied by conda
+    version = git_version_string
+    cwd = os.path.join(os.environ.get('RECIPE_DIR'),'..')
+else:
+    version = version2pep440(git_version_string)
+    cwd = os.getcwd()
+
+    
+print ('version {}'.format(version))
+print(cwd)
+fname = os.path.join(cwd, 'src/hello_world', 'version.py')
+
+if os.path.exists(fname):
+    os.remove(fname)
+with open(fname, 'w') as f:
+    f.write("version = \"{}\"".format(version))
+
+print ('version {}'.format(version))
 
 setup(
       name = "hello-world",
       description = 'Used for testing gh-actions',
-	version = dversion,
+	version = version,
 	packages = {'hello-world'},
       package_dir = {'hello-world': os.path.join('src','hello-world')},
       
