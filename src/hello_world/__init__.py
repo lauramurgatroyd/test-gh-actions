76a24ddd849a19f539cef3d97eb5f51e1b247d6d
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-11-03 16:33:08 +0000

    Delete __init__.py

--- src/hello_world/__init__.py
+++
@@ -1,8 +0,0 @@
-from os.path import dirname, basename, isfile, join
-import glob
-import sys
-
-# from https://stackoverflow.com/questions/1057431
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [basename(f)[:-3] for f in modules if isfile(f)
-           and not f.endswith('__init__.py')]
