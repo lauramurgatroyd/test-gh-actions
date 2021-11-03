84d8d5ac3f7367dcc4077eed1d042cf8736a3e70
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-11-03 16:37:28 +0000

    Don't copy directories

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,5 @@
 
-mkdir "%SRC_DIR%"
-ROBOCOPY /E "%RECIPE_DIR%\..\src" "%SRC_DIR%"
-# ROBOCOPY /E "%RECIPE_DIR%\..\test" "%SRC_DIR%\test"
-copy "%RECIPE_DIR%\..\setup.py" "%SRC_DIR%\setup.py"
-cd %SRC_DIR%
+cd %RECIPE_DIR%\..
 
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
