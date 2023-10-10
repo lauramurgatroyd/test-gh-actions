8fb0b3650460788064ac2285e5a27319870d8b07
Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
2023-10-10 09:07:52 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
 cd $RECIPE_DIR/..
 
-${PYTHON} setup.py install --single-version-externally-managed --record record.txt
+#${PYTHON} setup.py install --single-version-externally-managed --record record.txt
+pip install .
